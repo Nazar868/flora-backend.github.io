c83aeb5582417f72a3ed1465442c444576419863
Nazar868 <zagariynazar04@gmail.com>
2026-07-12 15:26:21 +0300

    Delete models directory

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require("../config/sequelize");
-const Bouquet = require("./bouquet");
-
-const syncModels = async () => {
-  // alter: true keeps the table in sync with the model during development
-  await sequelize.sync({ alter: true });
-};
-
-module.exports = { sequelize, Bouquet, syncModels };
